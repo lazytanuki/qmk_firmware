94c147f3e7a07baebd786f5f04dadcb9f2c8279a
LazyTanuki <43273245+lazytanuki@users.noreply.github.com>
2024-02-12 23:12:48 +0100

    feat(atreus62_ck): double tap lalt on lsft

--- keyboards/atreus62/rules.mk
+++ keyboards/atreus62/rules.mk
@@ -11,3 +11,4 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 #BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 UNICODE_ENABLE = yes         # Unicode
+TAP_DANCE_ENABLE = yes
